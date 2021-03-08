2473929a7e0ba087d042315c84329d1d0f62f048
unknown <kitey13579@gmail.com>
2021-03-08 19:52:37 +0000

    Added kick and mute commands

---
+++ commands/kick.js
@@ -0,0 +1,36 @@
+const Discord = require("discord.js")
+module.exports = {
+    name: "kick",
+    description: "kick command",
+    async execute(message, args, bot) {
+        if (!message.member.hasPermission("KICK_MEMBERS")) {
+            return message.channel.send("You cannot kick people.")
+        }
+        if (!message.guild.me.hasPermission("KICK_MEMBERS")) return message.channel.send("I do not have the correct permissions to mute.")
+        const target = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+        
+        if (target.roles.highest.position > message.guild.me.roles.highest.position) {
+                return message.channel.send('I cannot kick that user due to role hierarchy.');
+        }
+        if (!target) return message.channel.send("You need to tag the person you want to kick.")
+        if (target.id === message.author.id) return message.channel.send("You cannot kick yourself.")
+
+        let reason = args.slice(1).join(" ")
+        if (!reason) reason = "~~None.~~"
+
+        try {
+            const embed = new Discord.MessageEmbed()
+                .setTitle(`Successfully kicked ${target.user.tag}`)
+                .setColor('#000000')
+                .addField(`Moderator:`, `> ${message.author.tag}`)
+                .addField("Reason:", `> ${reason}`)
+                .setThumbnail(message.author.displayAvatarURL())
+
+            await target.kick()
+            return message.channel.send(embed)
+        } catch (err) {
+            console.log(err)
+            message.channel.send("Couldn't kick this user.")
+        }
+    }
+}
---
+++ commands/mute.js
@@ -0,0 +1,69 @@
+const ms = require("ms")
+const {
+    MessageEmbed
+} = require('discord.js');
+
+module.exports = {
+    "name": "mute",
+    "description": "mute people",
+    async execute(message, args, bot) {
+        if (!args) return message.reply("No time or reason specified.");
+        let moderator = message.author.tag
+        let time = args[1]
+        if (!ms(time)) return message.channel.send("No time provided.")
+        let reason = args.slice(2).join(" ")
+        console.log(reason)
+        if (!reason) {
+            return message.reply("No reason specified.");
+        }
+        let target = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+        if (!message.guild.me.hasPermission("MANAGE_MESSAGES")) return message.channel.send("I do not have the correct permissions to mute.")
+        if (!target) return message.reply("Couldn't find user.");
+        let muterole = message.guild.roles.cache.find(roles => roles.name === "Muted")
+        if (target.roles.cache.has(muterole.id)) {
+            return message.reply("That user is already muted!")
+        }
+        if (!muterole) {
+            try {
+                muterole = await message.guild.createRole({
+                    name: "muted",
+                    color: "#000000",
+                    permissions: []
+                })
+                message.guild.channels.forEach(async (channel, id) => {
+                    await channel.overwritePermissions(muterole, {
+                        SEND_MESSAGES: false,
+                        ADD_REACTIONS: false
+                    });
+                });
+            } catch (e) {
+                console.log(e);
+            }
+        }
+        await target.roles.add(muterole.id); // MUTED THEM
+        const embed = new MessageEmbed()
+            .setTitle(`${target.user.tag} was muted.`)
+            .addField("Muted By:", message.author.tag)
+            .addField("Time", `> ${time}`)
+            .setColor("#000000")
+            .addField('Reason: ', `> ${reason}`)
+
+        message.channel.send(embed)
+
+        try {
+            target.send(new MessageEmbed()
+                .setTitle(`You were muted by ${moderator}`)
+                .addField("For the reason: ", `> ${reason}`)
+                .addField("Time", `> ${time}`)
+                .setColor("#000000")
+                .setTimestamp())
+        } catch (err) {
+            message.channel.send("Unable to DM user about the moderation action. [probably disabled by them]")
+            console.error(err)
+        }
+        setTimeout(async () => {
+            await target.roles.remove(muterole.id)
+            console.log(`Successfully removed mute role from ${target.user.tag}.`)
+        }, ms(time))
+    }
+}
